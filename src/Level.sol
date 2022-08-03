6e9d3ce6c67afb7836072badf52f42aae45dd77f
0xrusowsky <0xrusowsky@proton.me>
2022-08-03 21:33:21 +0200

    Add Ethernaut contracts

--- src/Level.sol
+++ src/Level.sol
@@ -1,10 +1,9 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 
-pragma solidity ^0.6.0;
+import "solmate/auth/Owned.sol";
 
-import '@openzeppelin/contracts/access/Ownable.sol';
-
-abstract contract Level is Ownable {
-  function createInstance(address _player) virtual public payable returns (address);
-  function validateInstance(address payable _instance, address _player) virtual public returns (bool);
+abstract contract Level is Owned {
+    function createInstance(address _player) virtual public payable returns (address);
+    function validateInstance(address payable _instance, address _player) virtual public returns (bool);
 }
